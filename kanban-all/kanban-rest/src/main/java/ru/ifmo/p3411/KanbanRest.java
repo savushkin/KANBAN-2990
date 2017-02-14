3755290ef07b731e147358be834297b2725911c0
Ivan Savushkin <ivansavuskin@gmail.com>
2017-02-14 23:23:03 +0300

    KANBAN-35 Develop basic template

--- kanban-all/kanban-rest/src/main/java/ru/ifmo/p3411/KanbanRest.java
+++ kanban-all/kanban-rest/src/main/java/ru/ifmo/p3411/KanbanRest.java
@@ -2,6 +2,8 @@ package ru.ifmo.p3411;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 /**
@@ -9,7 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0
  */
 @SpringBootApplication
-public class KanbanRest {
+public class KanbanRest extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KanbanRest.class);
+    }
+
     public static void main(String... args) {
         SpringApplication.run(KanbanRest.class, args);
     }
